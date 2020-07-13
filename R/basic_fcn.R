1a21cddf27c5c637a9bbdd48e0c02b8acff06cba
Koohyun <bassdesire@gmail.com>
2020-07-13 16:56:45 -0400

    Restored basic_fcn.R // I will remove or save it separately later, in case it would be used by our another project

--- R/basic_fcn.R
+++ R/basic_fcn.R
@@ -1,3 +1,69 @@
+#' Short cut code for max(X,0)
+#' 
+#' Calculates component-wise max(X,0), where x can be a matrix.
+#'
+#' @param X A data matrix
+#'
+#' @return A matrix in the same dimension of X, after pmax(x,0) was applied to each row x of X.
+#' @export
+#'
+#' @examples
+pos <- function(X) {
+  return(pmax(X, 0))
+}
+
+#' Function for (x)_(V+)
+#'
+#' @param X A data matrix
+#' @param mon_ind Monotone variable index, whose length is equal to ncol(X)
+#'
+#' @return A matrix in the same dimension of X, after (x)_(V+) was applied to each row x of X.
+#' @export
+#'
+#' @examples
+Vplus <- function(X, mon_ind) {
+  
+  Xplus <- X
+  Xplus[, mon_ind] <- pos(X[, mon_ind])
+  
+  return(Xplus)
+}
+
+#' Function for (x)_(V-)
+#'
+#' @param X A numeric matrix
+#' @param mon_ind Monotone variable index, whose length is equal to ncol(X)
+#'
+#' @return A matrix in the same dimension of X, after (x)_(V-) was applied to each row x of X.
+#' @export
+#'
+#' @examples
+Vminus <- function(X, mon_ind) {
+  
+  return(Vplus(-X, mon_ind))
+}
+
+#' Function for L_p norm 
+#'
+#' @param X A numeric matrix (vector is not allowed)
+#' @param p Order of the norm
+#' @param invw Inverse weights for each component, with the length equal to either 1 or ncol(X)
+#'
+#' @return A vector of the dimension equal to the nrow(X)
+#' @export
+#'
+#' @examples
+Norm <- function(X, p = 2, invw = 1) {
+  
+  n <- nrow(X)
+  d <- ncol(X)
+  
+  invw <- matrix(rep(invw, each = n), nrow = n, ncol = d)
+  Xw <- X / invw
+  
+  return(rowSums(abs(Xw)^p)^(1/p))
+}
+
 maxminQ <- function(mu, sigma, alpha, simlen, lower){
   
   simdata <- MASS::mvrnorm(n = simlen, mu, sigma)
